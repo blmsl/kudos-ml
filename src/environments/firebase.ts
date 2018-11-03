acf68e405b2fefbc31fbc87d88f3bb639dc96c62
Jamie <15720274+tqm264@users.noreply.github.com>
2018-11-03 09:52:55 +0000

    Delete firebase.ts

--- src/environments/firebase.ts
+++
@@ -1,9 +0,0 @@
-
-  export const fbConfig = {
-    apiKey: 'AIzaSyCtubFPE-Ufb_nhCOlKgmwiaDBypj_oskI',
-    authDomain: 'kudosml-221208.firebaseapp.com',
-    databaseURL: 'https://kudosml-221208.firebaseio.com',
-    projectId: 'kudosml-221208',
-    storageBucket: 'kudosml-221208.appspot.com',
-    messagingSenderId: '1035748369623'
-  };
