3b5aae034de4601fed499997483fbc385de84942
Jamie <15720274+tqm264@users.noreply.github.com>
2018-11-11 20:13:04 +0000

    json testing

--- src/app/_core/_mock/_mock_site_data.ts
+++ src/app/_core/_mock/_mock_site_data.ts
@@ -1,2 +1,144 @@
 // Mock Data
 
+export const MockData = {
+
+  ex1: [
+    {
+      'Id': '1',
+      'Name': 'abc',
+      'Parent': '',
+      'attr': 'abc'
+    },
+    {
+      'Id': '2',
+      'Name': 'abc',
+      'Parent': '1',
+      'attr': 'abc'
+    },
+    {
+      'Id': '3',
+      'Name': 'abc',
+      'Parent': '2',
+      'attr': 'abc'
+    },
+    {
+      'Id': '4',
+      'Name': 'abc',
+      'Parent': '2',
+      'attr': 'abc'
+    }
+  ],
+
+  ex2: [
+    {
+      'grp11': 'Group 1',
+      'grp12': 'Group 1',
+      'grp13': 'Group 1',
+      grp21: 'Group 2',
+      grp22: 'Group 2',
+      grp23: 'Group 2',
+      grp31: 'Group 3',
+      grp32: 'Group 3',
+      grp33: 'Group 3'
+    }
+  ],
+
+  ex3: [
+    {
+      'UID': '215',
+      'POLYGON': 'M25_South',
+      'CHUNK': 'M25_South_IL_Camden',
+      'OPERATOR': 'VF',
+      'REGION': 'London',
+      'AREA_OP': 'London_VF',
+      'AREA_OWNER': 'London_East',
+      'Optimiser': 'Vasileios_Efstathiadis',
+      'Mobile_Optimiser': '7538185589',
+      'RadioManager': 'Paul_Crisp',
+      'Mobile_RadioManager': '7860362123',
+      'CSR': '8339', 'CELL_ID': '16485',
+      'SECTOR': '1',
+      'MI_MYCOM_ID': '1/1/008339',
+      'GENERATION': '3G',
+      'BCF_INT_ID': '0',
+      'BTS_INT_ID': '105',
+      'WCEL_ID': '8339011',
+      'FDD': '1',
+      'MODEL_NAME': '2100 Suburban 4.0',
+      'SITE_NAME': '57 CAREY STREET WC2A',
+      'SITE_TYPE': 'Micro Cell',
+      'CELL_TYPE': 'UMTS',
+      'MANUFACTURER': 'NOKIA',
+      'LACOD': '21002',
+      'NE_ID': '2',
+      'OMC_NE_NAME': 'BER-R002',
+      'MSC': '0',
+      'OMC_ID': 'RC3',
+      'CIRCUIT': 'EEPW12681891',
+      'CELL_CARRIERS': '0',
+      'CELL_3G_CARRIER': '10637',
+      'CALL_SAVER_CODE': '20',
+      'CELL_OF_ORIGIN': '8339',
+      'ZONE_CODE': '113',
+      'ADR1': '57 Carey Street',
+      'ADR2': '57 Carey Street',
+      'ADR3': ' ,',
+      'TOWN': 'London',
+      'COUNTY': 'GREATER LONDON',
+      'POSTCODE': 'WC2A 2JB',
+      'EASTING': '531052',
+      'NORTHING': '181264',
+      'BCCH': '0',
+      'ARFCN': '',
+      'TRX_LIST': '',
+      'NCC': '0',
+      'BCC': '0',
+      'SCC': '247',
+      'SAC': '10214',
+      'RAC': '135',
+      'TMA': '0',
+      'ENVIRONMENT': 'STSR',
+      'KIT_TYPE': 'UfK1111miX1A',
+      'ANTENNA_NUM': '1',
+      'BEAM_WIDTH': '75',
+      'ANT_NAME': 'C75X0M0**UMTS**',
+      'ANT_HEIGHT': '7',
+      'GROUND_HEIGHT': '0',
+      'TILT': '0',
+      'ELEC_TILT': '0',
+      'AZIMUTH': '170',
+      'POWER': '43',
+      'LOSSES': '3',
+      'AREA': 'London',
+      'REGION_2': 'UK - London',
+      'BIS_DATE': '20171231',
+      'SITE_GENERATION_TYPE': '3G',
+      'CELL_OWNER': 'TFinTF',
+      'JV_ID': 'T0009019H',
+      'CELL_SEQ_NO': '116764',
+      'ENODEB_ID': '0',
+      'PCI': '0',
+      'TAC': '0',
+      'RSI': '0',
+      'URA': '0',
+      'ENODEBNAME': '',
+      'PSEUDO_CARRIERS': '',
+      'BSC_C_NUMBER': '0',
+      'CISCO_BACKHAUL': ''
+    }
+  ],
+
+  ex4: [
+    {
+      'UID': '217', 'POLYGON': 'M25_South', 'CHUNK': 'M25_South_IL_CityofLondon', 'OPERATOR': 'VF', 'REGION': 'London', 'AREA_OP': 'London_VF', 'AREA_OWNER': 'London_East', 'Optimiser': 'Opt_7_J_W', 'Mobile_Optimiser': '', 'RadioManager': 'P_C', 'Mobile_RadioManager': '', 'CSR': '107', 'CELL_ID': '110', 'SECTOR': '1', 'MI_MYCOM_ID': '1/L1/000107', 'GENERATION': '4G', 'BCF_INT_ID': '0', 'BTS_INT_ID': '0', 'WCEL_ID': '0', 'FDD': '1', 'MODEL_NAME': '0800 Dense Urban 4.0', 'SITE_NAME': 'Broadway Circle', 'SITE_TYPE': 'Sectored', 'CELL_TYPE': 'LTE', 'MANUFACTURER': 'NOKIA', 'LACOD': '0', 'NE_ID': '0', 'OMC_NE_NAME': '', 'MSC': '0', 'OMC_ID': 'RC3', 'CIRCUIT': 'UNKNOWN', 'CELL_CARRIERS': '0', 'CELL_3G_CARRIER': '6400', 'CALL_SAVER_CODE': '', 'CELL_OF_ORIGIN': '', 'ZONE_CODE': '113', 'ADR1': '55 Old Broad Street', 'ADR2': '55 Old Broad Street', 'ADR3': '', 'TOWN': 'London', 'COUNTY': 'GREATER LONDON', 'POSTCODE': 'EC2M 1RX', 'EASTING': '533117', 'NORTHING': '181470', 'BCCH': '0', 'ARFCN': '', 'TRX_LIST': '', 'NCC': '0', 'BCC': '0', 'SCC': '0', 'SAC': '', 'RAC': '', 'TMA': '', 'ENVIRONMENT': '', 'KIT_TYPE': 'LfK31e000000X6', 'ANTENNA_NUM': '1', 'BEAM_WIDTH': '65', 'ANT_NAME': 'H65DUU33WW20C10**800**10**', 'ANT_HEIGHT': '39', 'GROUND_HEIGHT': '0', 'TILT': '5', 'ELEC_TILT': '10', 'AZIMUTH': '0', 'POWER': '43', 'LOSSES': '0', 'AREA': 'London', 'REGION_2': 'UK - London', 'BIS_DATE': '20171231', 'SITE_GENERATION_TYPE': '2G-3G-4G', 'CELL_OWNER': 'TFinTF', 'JV_ID': 'J0000038H', 'CELL_SEQ_NO': '306471', 'ENODEB_ID': '521864', 'PCI': '309', 'TAC': '14528', 'RSI': '26', 'URA': '0', 'ENODEBNAME': '', 'PSEUDO_CARRIERS': '', 'BSC_C_NUMBER': '0', 'CISCO_BACKHAUL': ''
+    },
+    {
+      'UID': '217', 'POLYGON': 'M25_South', 'CHUNK': 'M25_South_IL_CityofLondon', 'OPERATOR': 'VF', 'REGION': 'London', 'AREA_OP': 'London_VF', 'AREA_OWNER': 'London_East', 'Optimiser': 'Opt_7_J_W', 'Mobile_Optimiser': '', 'RadioManager': 'P_C', 'Mobile_RadioManager': '', 'CSR': '107', 'CELL_ID': '120', 'SECTOR': '2', 'MI_MYCOM_ID': '2/L1/000107', 'GENERATION': '4G', 'BCF_INT_ID': '0', 'BTS_INT_ID': '0', 'WCEL_ID': '0', 'FDD': '1', 'MODEL_NAME': '0800 Dense Urban 4.0', 'SITE_NAME': 'Broadway Circle', 'SITE_TYPE': 'Sectored', 'CELL_TYPE': 'LTE', 'MANUFACTURER': 'NOKIA', 'LACOD': '0', 'NE_ID': '0', 'OMC_NE_NAME': '', 'MSC': '0', 'OMC_ID': 'RC3', 'CIRCUIT': 'UNKNOWN', 'CELL_CARRIERS': '0', 'CELL_3G_CARRIER': '6400', 'CALL_SAVER_CODE': '', 'CELL_OF_ORIGIN': '', 'ZONE_CODE': '113', 'ADR1': '55 Old Broad Street', 'ADR2': '55 Old Broad Street', 'ADR3': '', 'TOWN': 'London', 'COUNTY': 'GREATER LONDON', 'POSTCODE': 'EC2M 1RX', 'EASTING': '533117', 'NORTHING': '181470', 'BCCH': '0', 'ARFCN': '', 'TRX_LIST': '', 'NCC': '0', 'BCC': '0', 'SCC': '0', 'SAC': '', 'RAC': '', 'TMA': '', 'ENVIRONMENT': '', 'KIT_TYPE': 'LfK31e000000X6', 'ANTENNA_NUM': '1', 'BEAM_WIDTH': '65', 'ANT_NAME': 'H65DUU33WW20C10**800**10**', 'ANT_HEIGHT': '39', 'GROUND_HEIGHT': '0', 'TILT': '3', 'ELEC_TILT': '10', 'AZIMUTH': '120', 'POWER': '43', 'LOSSES': '0', 'AREA': 'London', 'REGION_2': 'UK - London', 'BIS_DATE': '20171231', 'SITE_GENERATION_TYPE': '2G-3G-4G', 'CELL_OWNER': 'TFinTF', 'JV_ID': 'J0000038H', 'CELL_SEQ_NO': '306449', 'ENODEB_ID': '521864', 'PCI': '310', 'TAC': '14528', 'RSI': '26', 'URA': '0', 'ENODEBNAME': '', 'PSEUDO_CARRIERS': '', 'BSC_C_NUMBER': '0', 'CISCO_BACKHAUL': ''
+    },
+    {
+      'UID': '217', 'POLYGON': 'M25_South', 'CHUNK': 'M25_South_IL_CityofLondon', 'OPERATOR': 'VF', 'REGION': 'London', 'AREA_OP': 'London_VF', 'AREA_OWNER': 'London_East', 'Optimiser': 'Opt_7_J_W', 'Mobile_Optimiser': '', 'RadioManager': 'P_C', 'Mobile_RadioManager': '', 'CSR': '107', 'CELL_ID': '130', 'SECTOR': '3', 'MI_MYCOM_ID': '3/L1/000107', 'GENERATION': '4G', 'BCF_INT_ID': '0', 'BTS_INT_ID': '0', 'WCEL_ID': '0', 'FDD': '1', 'MODEL_NAME': '0800 Dense Urban 4.0', 'SITE_NAME': 'Broadway Circle', 'SITE_TYPE': 'Sectored', 'CELL_TYPE': 'LTE', 'MANUFACTURER': 'NOKIA', 'LACOD': '0', 'NE_ID': '0', 'OMC_NE_NAME': '', 'MSC': '0', 'OMC_ID': 'RC3', 'CIRCUIT': 'UNKNOWN', 'CELL_CARRIERS': '0', 'CELL_3G_CARRIER': '6400', 'CALL_SAVER_CODE': '', 'CELL_OF_ORIGIN': '', 'ZONE_CODE': '113', 'ADR1': '55 Old Broad Street', 'ADR2': '55 Old Broad Street', 'ADR3': '', 'TOWN': 'London', 'COUNTY': 'GREATER LONDON', 'POSTCODE': 'EC2M 1RX', 'EASTING': '533117', 'NORTHING': '181470', 'BCCH': '0', 'ARFCN': '', 'TRX_LIST': '', 'NCC': '0', 'BCC': '0', 'SCC': '0', 'SAC': '', 'RAC': '', 'TMA': '', 'ENVIRONMENT': '', 'KIT_TYPE': 'LfK31e000000X6', 'ANTENNA_NUM': '1', 'BEAM_WIDTH': '65', 'ANT_NAME': 'H65DUU33WW20C10**800**10**', 'ANT_HEIGHT': '39', 'GROUND_HEIGHT': '0', 'TILT': '6', 'ELEC_TILT': '10', 'AZIMUTH': '235', 'POWER': '43', 'LOSSES': '0', 'AREA': 'London', 'REGION_2': 'UK - London', 'BIS_DATE': '20171231', 'SITE_GENERATION_TYPE': '2G-3G-4G', 'CELL_OWNER': 'TFinTF', 'JV_ID': 'J0000038H', 'CELL_SEQ_NO': '306455', 'ENODEB_ID': '521864', 'PCI': '311', 'TAC': '14528', 'RSI': '26', 'URA': '0', 'ENODEBNAME': '', 'PSEUDO_CARRIERS': '', 'BSC_C_NUMBER': '0', 'CISCO_BACKHAUL': ''
+    }
+  ],
+
+
+};
--- src/app/testing/flat2json/flat2json.component.ts
+++ src/app/testing/flat2json/flat2json.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { MockData } from '../../_core/_mock/_mock_site_data';
 
 @Component({
   selector: 'app-flat2json',
@@ -8,105 +9,45 @@ import { Component, OnInit } from '@angular/core';
 export class Flat2jsonComponent implements OnInit {
 
 
-  ex1 = [
-    { 'Id': '1', 'Name': 'abc', 'Parent': '', 'attr': 'abc' },
-    { 'Id': '2', 'Name': 'abc', 'Parent': '1', 'attr': 'abc' },
-    { 'Id': '3', 'Name': 'abc', 'Parent': '2', 'attr': 'abc' },
-    { 'Id': '4', 'Name': 'abc', 'Parent': '2', 'attr': 'abc' }
-  ];
+  ex1 = MockData.ex1;
+  ex2 = MockData.ex2;
+  ex3 = MockData.ex3;
 
-  ex2 = [
-    {
-      'UID': '215',
-      'POLYGON': 'M25_South',
-      'CHUNK': 'M25_South_IL_Camden',
-      'OPERATOR': 'VF',
-      'REGION': 'London',
-      'AREA_OP': 'London_VF',
-      'AREA_OWNER': 'London_East',
-      'Optimiser': 'Vasileios_Efstathiadis',
-      'Mobile_Optimiser': '7538185589',
-      'RadioManager': 'Paul_Crisp',
-      'Mobile_RadioManager': '7860362123',
-      'CSR': '8339', 'CELL_ID': '16485',
-      'SECTOR': '1',
-      'MI_MYCOM_ID': '1/1/008339',
-      'GENERATION': '3G',
-      'BCF_INT_ID': '0',
-      'BTS_INT_ID': '105',
-      'WCEL_ID': '8339011',
-      'FDD': '1',
-      'MODEL_NAME': '2100 Suburban 4.0',
-      'SITE_NAME': '57 CAREY STREET WC2A',
-      'SITE_TYPE': 'Micro Cell',
-      'CELL_TYPE': 'UMTS',
-      'MANUFACTURER': 'NOKIA',
-      'LACOD': '21002',
-      'NE_ID': '2',
-      'OMC_NE_NAME': 'BER-R002',
-      'MSC': '0',
-      'OMC_ID': 'RC3',
-      'CIRCUIT': 'EEPW12681891',
-      'CELL_CARRIERS': '0',
-      'CELL_3G_CARRIER': '10637',
-      'CALL_SAVER_CODE': '20',
-      'CELL_OF_ORIGIN': '8339',
-      'ZONE_CODE': '113',
-      'ADR1': '57 Carey Street',
-      'ADR2': '57 Carey Street',
-      'ADR3': ' ,',
-      'TOWN': 'London',
-      'COUNTY': 'GREATER LONDON',
-      'POSTCODE': 'WC2A 2JB',
-      'EASTING': '531052',
-      'NORTHING': '181264',
-      'BCCH': '0',
-      'ARFCN': '',
-      'TRX_LIST': '',
-      'NCC': '0',
-      'BCC': '0',
-      'SCC': '247',
-      'SAC': '10214',
-      'RAC': '135',
-      'TMA': '0',
-      'ENVIRONMENT': 'STSR',
-      'KIT_TYPE': 'UfK1111miX1A',
-      'ANTENNA_NUM': '1',
-      'BEAM_WIDTH': '75',
-      'ANT_NAME': 'C75X0M0**UMTS**',
-      'ANT_HEIGHT': '7',
-      'GROUND_HEIGHT': '0',
-      'TILT': '0',
-      'ELEC_TILT': '0',
-      'AZIMUTH': '170',
-      'POWER': '43',
-      'LOSSES': '3',
-      'AREA': 'London',
-      'REGION_2': 'UK - London',
-      'BIS_DATE': '20171231',
-      'SITE_GENERATION_TYPE': '3G',
-      'CELL_OWNER': 'TFinTF',
-      'JV_ID': 'T0009019H',
-      'CELL_SEQ_NO': '116764',
-      'ENODEB_ID': '0',
-      'PCI': '0',
-      'TAC': '0',
-      'RSI': '0',
-      'URA': '0',
-      'ENODEBNAME': '',
-      'PSEUDO_CARRIERS': '',
-      'BSC_C_NUMBER': '0',
-      'CISCO_BACKHAUL': ''
-    }
-  ];
+
 
   result1;
+  result2;
+  result3;
 
   constructor () { }
 
   ngOnInit() {
     this.result1 = this.convert(this.ex1);
+    this.result2 = this.convert(this.ex2);
+    this.result3 = this.convert(this.ex3);
+
+    // const { grp31, grp21, grp11, ...others } = this.ex2[0];
+
+    // console.log('Group 1', grp11);
+    // console.log('Group 2', grp21);
+    // console.log('Group 3', grp31);
+    // console.log('REST', others);
+
+    const test = { 'UID': '215', 'POLYGON': 'M25_South', 'CHUNK': 'M25_South_IL_Camden', 'OPERATOR': 'VF', 'REGION': 'London' };
+
+    const { CHUNK, UID, ...remain  } = test;
+
+    console.log('CHUNK', CHUNK);
+    console.log('UID', UID);
+    console.log('REST', remain);
+
+    const { POLYGON, REGION, ...remain2 } = remain;
+
+    console.log('POLYGON', POLYGON);
+    console.log('REGION', REGION);
+    console.log('REST2', remain2);
   }
+
 
 
   convert(array) {
