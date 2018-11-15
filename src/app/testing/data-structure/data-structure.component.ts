50161aacc9f25daf01782721c4ebb1e0568e156a
Jamie <15720274+tqm264@users.noreply.github.com>
2018-11-15 07:34:14 +0000

    renameKeys

--- src/app/testing/data-structure/data-structure.component.ts
+++ src/app/testing/data-structure/data-structure.component.ts
@@ -18,9 +18,11 @@ export class DataStructureComponent implements OnInit {
     ['ac', 'AC'],
   ]);
 
+
+
   constructor () {
     this.input = MockData.ex3;
-   }
+  }
 
   ngOnInit() {
   }
@@ -29,6 +31,48 @@ export class DataStructureComponent implements OnInit {
     console.log('Start');
     // this.output = this.dmap.entries();
     // console.log(this.dmap.keys());
+    const test = { name: 'JAMIE', surname: 'Worsfold', location: 'London', age: 43 };
+    const new1 = this.renameProp('name', 'forename', test);
+    const new2 = this.renameKeys({ 'surname': 'LASTNAME', 'name': 'FIRSTNAME' }, test);
+
+    console.log(['old', test], ['new', new1], ['new2', new2]);
   }
+
+  renameKeys = (keysMap, obj) => {
+
+    // Usage
+    //   renameKeys({
+    //     name: 'firstName',
+    //     job: 'passion'
+    // }, {
+    //     name: 'Bobo',
+    //     job: 'Front-End Master'
+    // });
+
+    debugger;
+
+    return Object
+      .keys(obj)
+      .reduce((acc, key) => {
+        debugger;
+
+        const renamedObject = {
+          [keysMap[key] || key]: obj[key]
+        };
+
+        debugger;
+
+        return {
+          ...acc,
+          ...renamedObject
+        }
+      }, {});
+  };
+
+
+  renameProp = (oldProp, newProp, { [oldProp]: oldVal, ...others }) => ({
+    [newProp]: oldVal,
+    ...others
+  })
 
 }
