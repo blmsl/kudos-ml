3be4fe5200f56b194d20e844a3f0035f3b41a13f
Jamie <15720274+tqm264@users.noreply.github.com>
2018-11-25 15:27:43 +0000

    observe progress

--- src/app/testing/csv2json/csv2json.component.ts
+++ src/app/testing/csv2json/csv2json.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { BehaviorSubject, Observable, of } from 'rxjs';
 import { CsvService } from '../../_core/_services/csv.service';
 import { FirestoreService } from '../../_core/_services/firestore.service';
+import { tick } from '@angular/core/src/render3';
 
 
 interface FileEvent {
@@ -11,11 +13,11 @@ interface FileEvent {
 }
 
 interface BucketProgress {
-  bucket: {
-    progress: number;
-    total: number;
-  };
+  pcnt: number;
+  progress: number;
+  total: number;
 }
+
 
 
 @Component({
@@ -29,15 +31,49 @@ export class Csv2jsonComponent implements OnInit {
   ready: Boolean = false;
   hideProgBar: Boolean = false;
 
-  // progStates: BucketProgress[]; // bucket: {prog: 0, total:0}
   progStates = {};
+  progState$: BehaviorSubject<any> = new BehaviorSubject(null);
+  progObs: Observable<any>;
 
-  constructor (private parseCsvSvc: CsvService, private kfs: FirestoreService) { }
+  sitesObj: BucketProgress = { pcnt: 0, progress: 0, total: 0 };
+  sites$: BehaviorSubject<BucketProgress> = new BehaviorSubject(this.sitesObj);
 
-  ngOnInit() { }
+
+  constructor (private parseCsvSvc: CsvService, private kfs: FirestoreService) {
+
+
+  }
+
+  ngOnInit() {
+
+
+    // this.progObs = of(this.progStates);
+
+    // this.progObs.subscribe(data => {
+    //   console.log('SUBSCRIBE', data);
+    // });
+
+
+  }
 
 
   async startParse() {
+
+
+    this.sites$.subscribe(val => {
+      console.log('Sites Subsc', val);
+    });
+
+    this.progState$.subscribe(data => {
+
+      if (data !== null) {
+        // console.log('SUBSCRIBE BHS', data);
+        if (data.hasOwnProperty('sites')) {
+          this.sites$.next(data.sites);
+        }
+      }
+
+    });
 
     this.hideProgBar = false;
 
@@ -48,7 +84,8 @@ export class Csv2jsonComponent implements OnInit {
       Object.keys(parsedData).forEach(group => {
         // processedCount[group] = parsedData[group].length;
         const total = parsedData[group].length;
-        this.progStates[group] = { progress: 0, total };
+        const bucketProg: BucketProgress = { pcnt: 0, progress: 0, total };
+        this.progStates[group] = bucketProg;
       });
 
       this.uploadData(parsedData);
@@ -75,15 +112,31 @@ export class Csv2jsonComponent implements OnInit {
     Object.entries(upData).forEach(([bucket, batch]) => {
 
       const dbCol = baseCollection + bucket;
+      let counter = 0;
 
       for (const key in batch) {
         if (batch.hasOwnProperty(key)) {
-          this.kfs.createDocument(dbCol, batch[key]);
+          // this.kfs.createDocument(dbCol, batch[key]);
+          const total = this.progStates[bucket].total;
+          counter++;
+          this.updateProgress(bucket, counter, total);
         }
       }
 
     });
 
+  }
+
+  private updateProgress(bucket, progress, total) {
+    const pcnt = (progress / total) * 100;
+    const bcktProgress: BucketProgress = {
+      pcnt,
+      progress,
+      total
+    };
+    this.progStates[bucket] = bcktProgress;
+    this.progState$.next(this.progStates);
+    // console.log(bucket, bcktProgress);
   }
 
 
